5e6b05bf301b86304847991583156a5384e1bc96
Jason Gilholme <jasongh@lumapictures.com>
2020-02-23 11:36:55 +1100

    Add python binding lib as second dimension to test matrix.

--- iconify/qt.py
+++ iconify/qt.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from typing import *
     from PySide2 import QtCore, QtGui, QtSvg, QtWidgets
+    qtlib = 'PySide2'
 else:
     qtlib = os.environ.get("ICONIFY_QTLIB", "PySide2")
 
@@ -15,17 +16,17 @@ else:
 
 
 _IMPORT_ERROR_MESSAGE = \
-    "Unable to import {0}! Please set the 'ICONIFY_QTLIB' env var " \
+    "Unable to import {0}.{1}! Please set the 'ICONIFY_QTLIB' env var " \
     "to the location of the Qt binding you would like to use."
 
 if QtCore is None:
-    raise ImportError(_IMPORT_ERROR_MESSAGE.format('QtCore'))
+    raise ImportError(_IMPORT_ERROR_MESSAGE.format(qtlib, 'QtCore'))
 
 if QtGui is None:
-    raise ImportError(_IMPORT_ERROR_MESSAGE.format('QtGui'))
+    raise ImportError(_IMPORT_ERROR_MESSAGE.format(qtlib, 'QtGui'))
 
 if QtSvg is None:
-    raise ImportError(_IMPORT_ERROR_MESSAGE.format('QtSvg'))
+    raise ImportError(_IMPORT_ERROR_MESSAGE.format(qtlib, 'QtSvg'))
 
 if QtWidgets is None:
-    raise ImportError(_IMPORT_ERROR_MESSAGE.format('QtWidgets'))
+    raise ImportError(_IMPORT_ERROR_MESSAGE.format(qtlib, 'QtWidgets'))
