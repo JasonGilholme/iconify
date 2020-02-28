c252bed861c15c5fc68fd8a471e649cefa875e6d
Jason Gilholme <jasongh@lumapictures.com>
2020-02-28 21:32:08 +1100

    Add dependency versions.  Re-enable tests.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,38 +1,38 @@
-#
-# import iconify
-# from iconify.qt import QtCore, QtGui, QtWidgets
-#
-#
-# def test_icon(qtbot, validIconPath):
-#     anim = iconify.anim.Spin()
-#     icon = iconify.Icon(
-#         'delete',
-#         color=QtGui.QColor('red'),
-#         anim=anim,
-#     )
-#
-#     button = QtWidgets.QPushButton()
-#     button.setIcon(icon)
-#     button.clicked.connect(anim.toggle)
-#
-#     button.show()
-#
-#     qtbot.mouseClick(button, QtCore.Qt.LeftButton)
-#
-#
-# def test_pixmapGenerator(validIconPath):
-#     iconPath = iconify.path.findIcon('delete')
-#
-#     anim = iconify.anim.Spin()
-#     pixGen = iconify.core.PixmapGenerator(iconPath, color=QtGui.QColor('blue'), anim=anim)
-#
-#     size = QtCore.QSize(24, 24)
-#     altSize = QtCore.QSize(32, 32)
-#
-#     # Ensure that the pixmap generators cache is working as expected
-#     pixmapA = pixGen.pixmap(size)
-#     pixmapB = pixGen.pixmap(size)
-#     pixmapC = pixGen.pixmap(altSize)
-#
-#     assert pixmapA is pixmapB
-#     assert pixmapA is not pixmapC
+
+import iconify
+from iconify.qt import QtCore, QtGui, QtWidgets
+
+
+def test_icon(qtbot, validIconPath):
+    anim = iconify.anim.Spin()
+    icon = iconify.Icon(
+        'delete',
+        color=QtGui.QColor('red'),
+        anim=anim,
+    )
+
+    button = QtWidgets.QPushButton()
+    button.setIcon(icon)
+    button.clicked.connect(anim.toggle)
+
+    button.show()
+
+    qtbot.mouseClick(button, QtCore.Qt.LeftButton)
+
+
+def test_pixmapGenerator(validIconPath):
+    iconPath = iconify.path.findIcon('delete')
+
+    anim = iconify.anim.Spin()
+    pixGen = iconify.core.PixmapGenerator(iconPath, color=QtGui.QColor('blue'), anim=anim)
+
+    size = QtCore.QSize(24, 24)
+    altSize = QtCore.QSize(32, 32)
+
+    # Ensure that the pixmap generators cache is working as expected
+    pixmapA = pixGen.pixmap(size)
+    pixmapB = pixGen.pixmap(size)
+    pixmapC = pixGen.pixmap(altSize)
+
+    assert pixmapA is pixmapB
+    assert pixmapA is not pixmapC
